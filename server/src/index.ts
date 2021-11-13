4906c582b43076e95601d0c6c43247d822dbafc9
azinko <adrian.zinko@bitgrip.de>
2021-11-13 11:08:45 +0100

    Configurate apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,32 @@
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+
 import { __prod__ } from "./constants";
 import { Article } from "./entities/Article";
 import mikroOrmConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(5000, () => {
+    console.log("Server is listening at http://localhost:5000");
+  });
+
   const article = orm.em.create(Article, { title: "new article" });
   await orm.em.persistAndFlush(article);
 };
