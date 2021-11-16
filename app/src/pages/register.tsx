018a6c83b0e217e1d05701c2aca41216f555c2e3
Adrian Zinko <adrian.zinko@billennium.com>
2021-11-16 14:48:20 +0100

    add comment

--- app/src/pages/register.tsx
+++ app/src/pages/register.tsx
@@ -25,6 +25,7 @@ const Register: React.FC<registerProps> = ({}) => {
             // @ts-ignore
             setErrors(toErrorMap(response.data.register.errors));
           } else if (response.data?.register.user) {
+            // redirect to main page
             router.push("/");
           }
         }}
