5b5d60905045f09389885af8587ea9de7af2b2e6
Adrian Zinko <adrian.zinko@billennium.com>
2021-11-16 08:18:32 +0100

    create mutation through graphql

--- server/src/resolvers/article.ts
+++ server/src/resolvers/article.ts
@@ -1,6 +1,6 @@
 import { Article } from "../entities/Article";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class ArticleResolver {
@@ -15,5 +15,42 @@ export class ArticleResolver {
     @Ctx() { em }: MyContext
     ): Promise<Article | null> {
     return em.findOne(Article, { _id: id });
+  }
+
+  @Mutation(() => Article)
+  async createArticle(
+    // @Arg('title', () => String) title: string,
+    @Arg('title', () => String) title: string,
+    @Ctx() { em }: MyContext
+    ): Promise<Article> {
+    const article = em.create(Article, { title })
+    await em.persistAndFlush(article);
+    return article;
+  }
+
+  @Mutation(() => Article, { nullable: true })
+  async updateArticle(
+    @Arg("id") id: number,
+    @Arg('title', () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+    ): Promise<Article | null> {
+    const article = await em.findOne(Article, { _id: id });
+    if (!article) {
+      return null;
+    }
+    if (typeof title !== 'undefined') {
+      article.title = title;
+      await em.persistAndFlush(article);
+    }
+    return article;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteArticle(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+    ): Promise<boolean> {
+    em.nativeDelete(Article, { _id: id });
+    return true;
   }
 }
