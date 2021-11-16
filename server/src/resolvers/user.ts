26e1241e01fb18904c7d627afb38ccd8f51935c6
Adrian Zinko <adrian.zinko@billennium.com>
2021-11-16 10:02:39 +0100

    handle login errors

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -30,15 +30,45 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UserPasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [{
+          field: 'username',
+          message: 'length must be greater than 2 chars'
+        }]
+      }
+    }
+    if (options.password.length <= 6) {
+      return {
+        errors: [{
+          field: 'password',
+          message: 'length must be greater than 6 chars'
+        }]
+      }
+    }
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, { username: options.username, password: hashedPassword });
-    await em.persistAndFlush(user);
-    return user;
+    try {
+      await em.persistAndFlush(user);      
+    } catch (error) {
+      if (error.code === '23505' || error.details.includes('already exists')) {
+        // duplicate username error
+        return {
+          errors: [{
+            field: "username",
+            message: "username has already been taken"
+          }]
+        }
+      }
+    }
+      return { 
+        user,
+      };
   }
 
   @Mutation(() => UserResponse)
