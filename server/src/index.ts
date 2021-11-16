1d729caea607482d3c22481aff30eb57e05865ac
Adrian Zinko <adrian.zinko@billennium.com>
2021-11-16 10:29:48 +0100

    connect to redis db

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import { MikroORM } from "@mikro-orm/core";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import { buildSchema } from "type-graphql";
+import connectRedis from "connect-redis";
 
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
@@ -9,18 +10,44 @@ import { ArticleResolver } from "./resolvers/article";
 import { InitResolver } from "./resolvers/init";
 import { UserResolver } from "./resolvers/user";
 
+import redis from "redis";
+import session from "express-session";
+import { MyContext } from "./types";
+
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
   const app = express();
 
+  let RedisStore = connectRedis(session);
+  let redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "sessionId",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // protecting from csrf
+        secure: __prod__, // set cookie work only for https
+      },
+      saveUninitialized: false,
+      secret: "22daawaadaawaw3d", // random string for now
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [InitResolver, ArticleResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
